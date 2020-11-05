5d700416cec2a82dd6efb7c41528d835529c1c1a
zmonty <zmonty@cloudbolt.io>
2020-11-05 17:44:17 -0500

    added host

--- src/settings.py
+++ src/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'Optional default value')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'https://twitclone-backend.herokuapp.com/'
+]
 
 
 # Application definition
